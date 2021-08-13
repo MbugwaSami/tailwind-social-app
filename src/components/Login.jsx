e3b6d706fe809c68d9b62d2049aaf5a6ba48725d
Sammy Njau <v-sanjau@microsoft.com>
2021-08-13 10:28:09 +0300

    Task 2: Form Validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,15 @@
 import { useHistory } from "react-router-dom";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 export default function Login() {
   const history = useHistory();
+  const loginSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Email address is invalid")
+      .required("Email address is required"),
+    password: Yup.string().required("Password is required"),
+  });
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
@@ -12,60 +20,88 @@ export default function Login() {
           <p className="mt-2 text-center text-sm text-gray-600">
             Or{" "}
             <a
-              onClick={()=>history.push('/signup')}
+              onClick={() => history.push("/signup")}
               className="font-medium text-blue-600 hover:text-blue-500 cursor-pointer"
             >
               Create Account
             </a>
           </p>
         </div>
-        <form className="mt-8 space-y-6">
-          <input type="hidden" name="remember" defaultValue="true" />
-          <div className="rounded-md shadow-sm -space-y-px">
-            <div>
-              <label htmlFor="email-address">Email address</label>
-              <input
-                id="email-address"
-                name="email"
-                type="email"
-                required
-                className="w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                placeholder="Email address"
-              />
-            </div>
-            <div>
-              <label htmlFor="password">Password</label>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                required
-                className="w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                placeholder="Password"
-              />
-            </div>
-          </div>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={loginSchema}
+          onSubmit={async (values) => {
+            console.log(values, "value");
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+          }) => (
+            <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+              <input type="hidden" name="remember" defaultValue="true" />
+              <div className="rounded-md shadow-sm -space-y-px">
+                <div>
+                  <label htmlFor="email-address">Email address</label>
+                  <input
+                    id="email-address"
+                    name="email"
+                    type="email"
+                    className="w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                    placeholder="Email address"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    onFocus={handleBlur}
+                  />
+                  {errors.email && touched.email && (
+                    <p class="text-red-500 text-sm italic">{errors.email}</p>
+                  )}
+                </div>
+                <div>
+                  <label htmlFor="password">Password</label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    className="w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                    placeholder="Password"
+                    value={values.password}
+                    onChange={handleChange}
+                  />
+                  {errors.password && touched.password && (
+                    <p class="text-red-500 text-sm italic">{errors.password}</p>
+                  )}
+                </div>
+              </div>
 
-          <div className="flex items-center justify-end">
-            <div className="text-sm">
-              <a
-                href="#"
-                className="font-medium text-blue-600 hover:text-blue-500"
-              >
-                Forgot your password?
-              </a>
-            </div>
-          </div>
+              <div className="flex items-center justify-end">
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-medium text-blue-600 hover:text-blue-500"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+              </div>
 
-          <div>
-            <button
-              type="submit"
-              className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            >
-              Sign in
-            </button>
-          </div>
-        </form>
+              <div>
+                <button
+                  type="submit"
+                  className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                >
+                  {isSubmitting ? "Loading..." : "Sign in"}
+                </button>
+              </div>
+            </form>
+          )}
+        </Formik>
       </div>
     </div>
   );
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,4 +1,28 @@
+import * as Yup from "yup";
+import { Formik } from "formik";
+
 export default function SignUp() {
+  const phoneRegExp = /^(2547)([0-9|7])(\d){7}$/;
+  const SignupSchema = Yup.object().shape({
+    firstName: Yup.string()
+      .required("First name is required")
+      .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed  ")
+      .min(3, "Too Short!")
+      .max(10, "Too Long!"),
+    lastName: Yup.string()
+      .required("Last name is required")
+      .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed  ")
+      .min(3, "Too Short!")
+      .max(10, "Too Long!"),
+    email: Yup.string()
+      .email("Email address is invalid")
+      .required("Email address is required"),
+    password: Yup.string().required("Password is required"),
+    confirm: Yup.string().required("Confirm password is required"),
+    phone: Yup.string()
+      .required("Phone is required")
+      .matches(phoneRegExp, "Phone number is not valid"),
+  });
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
@@ -7,85 +31,152 @@ export default function SignUp() {
             Create your account
           </h2>
         </div>
-        <form className="mt-8 space-y-6">
-          <input type="hidden" name="remember" defaultValue="true" />
-          <div className="rounded-md shadow-sm -space-y-px">
-            <div>
-              <label htmlFor="first-name">First name</label>
-              <input
-                id="first-name"
-                name="firstName"
-                type="text"
-                required
-                className="w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                placeholder="Enter first name"
-              />
-            </div>
-            <div>
-              <label htmlFor="last-name">Last name</label>
-              <input
-                id="last-name"
-                name="lastName"
-                type="text"
-                required
-                className="w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                placeholder="Enter last name"
-              />
-            </div>
-            <div>
-              <label htmlFor="phone">Phone</label>
-              <input
-                id="phone"
-                name="phone"
-                type="text"
-                required
-                className="w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                placeholder="Enter phone number"
-              />
-            </div>
-            <div>
-              <label htmlFor="email-address">Enter email address</label>
-              <input
-                id="email-address"
-                name="email"
-                type="email"
-                required
-                className="w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                placeholder="Email address"
-              />
-            </div>
-            <div>
-              <label htmlFor="password">Password</label>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                required
-                className="w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                placeholder="Enter password"
-              />
-            </div>
-            <div>
-              <label htmlFor="password">Confirm password</label>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                required
-                className="w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                placeholder="Confirm password"
-              />
-            </div>
-          </div>
-          <div>
-            <button
-              type="submit"
-              className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            >
-              Sign up
-            </button>
-          </div>
-        </form>
+        <Formik
+          initialValues={{
+            firstName: "",
+            lastName: "",
+            email: "",
+            phone: "",
+            password: "",
+            confirm: "",
+          }}
+          validationSchema={SignupSchema}
+          validate={(values) => {
+            const errors = {};
+            const { password, confirm } = values;
+            if (password && confirm) {
+              if (password !== confirm) {
+                errors.confirm = "Passwords do not match";
+              }
+            }
+            return errors;
+          }}
+          onSubmit={async (values) => {
+            console.log(values, "value");
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+          }) => (
+            <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+              <input type="hidden" name="remember" defaultValue="true" />
+              <div className="rounded-md shadow-sm -space-y-px">
+                <div>
+                  <label htmlFor="first-name">First name</label>
+                  <input
+                    id="first-name"
+                    name="firstName"
+                    type="text"
+                    className="w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                    placeholder="Enter first name"
+                    value={values.firstName}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.firstName && touched.firstName && (
+                    <p class="text-red-500 text-sm italic">
+                      {errors.firstName}
+                    </p>
+                  )}
+                </div>
+                <div>
+                  <label htmlFor="last-name">Last name</label>
+                  <input
+                    id="last-name"
+                    name="lastName"
+                    type="text"
+                    className="w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                    placeholder="Enter last name"
+                    value={values.lastName}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.lastName && touched.lastName && (
+                    <p class="text-red-500 text-sm italic">{errors.lastName}</p>
+                  )}
+                </div>
+                <div>
+                  <label htmlFor="phone">Phone</label>
+                  <input
+                    id="phone"
+                    name="phone"
+                    type="text"
+                    className="w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                    placeholder="Phone number eg 2547247890"
+                    value={values.phone}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.phone && touched.phone && (
+                    <p class="text-red-500 text-sm italic">{errors.phone}</p>
+                  )}
+                </div>
+                <div>
+                  <label htmlFor="email-address">Enter email address</label>
+                  <input
+                    id="email-address"
+                    name="email"
+                    type="email"
+                    className="w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                    placeholder="Email address"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.email && touched.email && (
+                    <p class="text-red-500 text-sm italic">{errors.email}</p>
+                  )}
+                </div>
+                <div>
+                  <label htmlFor="password">Password</label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    className="w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                    placeholder="Enter password"
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.password && touched.password && (
+                    <p class="text-red-500 text-sm italic">{errors.password}</p>
+                  )}
+                </div>
+                <div>
+                  <label htmlFor="confirm">Confirm password</label>
+                  <input
+                    id="confirm"
+                    name="confirm"
+                    type="password"
+                    className="w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                    placeholder="Confirm password"
+                    value={values.confirm}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.confirm && touched.confirm && (
+                    <p class="text-red-500 text-sm italic">{errors.confirm}</p>
+                  )}
+                </div>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                >
+                  {isSubmitting ? "Saving..." : "Sign up"}
+                </button>
+              </div>
+            </form>
+          )}
+        </Formik>
       </div>
     </div>
   );
